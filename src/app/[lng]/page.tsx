b83295d4f0f4005395fd4fb06cb11b51541a2525
rdyakov <rosend@anvilo.com>
2023-09-18 16:09:59 +0300

    feat: server-side localization, linting, prettier

---
+++ src/app/[lng]/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { useTranslation } from '../i18n'
+import { Footer } from '../components/Footer'
+
+export default async function Page({
+  params: { lng },
+}: {
+  params: { lng: string }
+}) {
+  const { t } = await useTranslation(lng)
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      <Link href={`/${lng}/i18next-app-router`}>{t('to-second-page')}</Link>
+      <Footer lng={lng} />
+    </>
+  )
+}
